a6f646f7fb37352913feefea10ba09daa1321aa5
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-02-27 20:44:55 -0800

    Server can call its own API using protobufs.

--- server/src/test/java/org/davincischools/leo/server/ServerApplicationTest.java
+++ server/src/test/java/org/davincischools/leo/server/ServerApplicationTest.java
@@ -3,24 +3,40 @@ package org.davincischools.leo.server;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.davincischools.leo.server.SpringConstants.LOCAL_SERVER_PORT_PROPERTY;
 
+import org.davincischools.leo.proto.greeter.HelloReply;
+import org.davincischools.leo.proto.greeter.HelloRequest;
 import org.davincischools.leo.server.controllers.ReactResourceController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ServerApplicationTest {
   @Autowired private ReactResourceController controller;
+  @Autowired private TestRestTemplate restTemplate;
 
   @Value(value = "${" + LOCAL_SERVER_PORT_PROPERTY + "}")
   private int port;
 
-  @Autowired private TestRestTemplate restTemplate;
+  @Before
+  public void setUp() {
+    // The configuration in ServerApplication.ServerApplicationConfigurer is
+    // not applied in the test instance. So, we need to apply it manually.
+    //
+    // I tried referring to the class using the test class annotations. But,
+    // that didn't seem to work. But, that would be the preferred solution.
+    ServerApplication.ServerApplicationConfigurer configurer =
+        new ServerApplication.ServerApplicationConfigurer();
+    configurer.extendMessageConverters(restTemplate.getRestTemplate().getMessageConverters());
+  }
 
   @Test
   public void controllerLoadsTest() {
@@ -31,5 +47,16 @@ public class ServerApplicationTest {
   public void indexPageLoadsTest() {
     assertThat(restTemplate.getForObject("http://localhost:" + port + "/", String.class))
         .contains("Leo");
+  }
+
+  @Test
+  public void greeterServiceTest() throws Exception {
+    ResponseEntity<HelloReply> reply =
+        restTemplate.postForEntity(
+            "http://localhost:" + port + "/api/Greeter",
+            HelloRequest.newBuilder().setGreeterName("Test").build(),
+            HelloReply.class);
+    assertThat(reply.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(reply.getBody().getReplyMessage()).isEqualTo("Hello Test");
   }
 }
