53c0665495d4de4acf52078653589e83e56b7ce4
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-02-27 20:44:55 -0800

    Server can call its own API using protobufs.

--- server/src/main/java/org/davincischools/leo/server/ServerApplication.java
+++ server/src/main/java/org/davincischools/leo/server/ServerApplication.java
@@ -7,16 +7,36 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.http.converter.protobuf.ProtobufJsonFormatHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
 public class ServerApplication {
   private static final Logger log = LogManager.getLogger();
+
+  @Configuration
+  static class ServerApplicationConfigurer extends WebMvcConfigurationSupport {
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+      // Add the Protobuf converters to the list of converters.
+      //
+      // These need to be at the beginning of the list to take precedence over
+      // other converters. Otherwise, a different, built-in converter fails
+      // with a "Direct self-reference leading to cycle" error.
+      converters.add(0, new ProtobufHttpMessageConverter());
+      converters.add(1, new ProtobufJsonFormatHttpMessageConverter());
+    }
+  }
 
   public static void main(String[] args) throws IOException {
     CommandLineArguments.initialize(args);
