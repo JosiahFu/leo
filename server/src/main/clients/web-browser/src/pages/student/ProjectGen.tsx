44000740af693b0e060e26cb809056237b5bd8e9
manorajesh <mano@raje.sh>
2023-03-02 22:38:01 -0800

    Simplify code by removing irrelevant comments

--- server/src/main/clients/web-browser/src/pages/student/ProjectGen.tsx
+++ server/src/main/clients/web-browser/src/pages/student/ProjectGen.tsx
@@ -10,18 +10,10 @@ function App() {
     alignItems: 'center',
   };
 
-  // Unused variable input will be used in API call in future
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
   const processInput = async (input: stringSet): Promise<stringSet> => {
     const completion = await getCompletion(input);
     return completion;
-
-    // return [
-    //   'Exponential Function in Video Games: Have students analyze the way exponential functions are used in video games, such as in scoring systems or character growth. They can then create their own video game using exponential functions, incorporating programming skills to bring the game to life.',
-    //   'Engineering Design Challenge: Have students design and build a system using exponential functions, such as a solar panel array or a machine that performs a task exponentially faster over time. Students can use programming to control and monitor the performance of their system.',
-    //   'Function Transformations in Physics: Have students explore how exponential functions can model real-world phenomena, such as radioactive decay or the spread of a disease. They can then use function transformations and programming skills to analyze and predict these processes.',
-    //   'Cost-Benefit Analysis for Business: Have students use exponential functions to model the costs and benefits of a new product or technology. They can then use programming skills to create interactive visualizations or simulations to communicate their findings.',
-    // ];
   };
 
   return (
