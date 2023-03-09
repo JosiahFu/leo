c046fdb9cf7608cc0626b5af92ebd8fc8a06350e
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-03-09 10:51:13 -0800

    Remove unnecessary test code in OpenAiUtilsTest.

--- server/src/test/java/org/davincischools/leo/server/utils/OpenAiUtilsTest.java
+++ server/src/test/java/org/davincischools/leo/server/utils/OpenAiUtilsTest.java
@@ -10,12 +10,7 @@ import org.davincischools.leo.protos.open_ai.CreateCompletionResponse;
 import org.davincischools.leo.server.ServerApplication;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@RunWith(SpringJUnit4ClassRunner.class)
 public class OpenAiUtilsTest {
 
   @Before
