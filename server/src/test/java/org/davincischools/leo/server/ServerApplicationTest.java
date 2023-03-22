cb2e8568e0a01b74bc49bab52a788b0579b82657
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-03-21 20:22:53 -0700

    Fix compile error due to not using Optional<>.

--- server/src/test/java/org/davincischools/leo/server/ServerApplicationTest.java
+++ server/src/test/java/org/davincischools/leo/server/ServerApplicationTest.java
@@ -1,9 +1,11 @@
 package org.davincischools.leo.server;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth8.assertThat;
 import static org.davincischools.leo.database.post_environment_processors.ConfigureTestDatabaseEnvironmentPostProcessor.USE_TEST_DATABASE;
 import static org.davincischools.leo.server.SpringConstants.LOCAL_SERVER_PORT_PROPERTY;
 
+import java.util.Optional;
 import org.davincischools.leo.database.daos.Database;
 import org.davincischools.leo.database.daos.User;
 import org.davincischools.leo.database.test.TestData;
@@ -94,8 +96,8 @@ public class ServerApplicationTest {
 
   @Test
   public void usersAddedTest() {
-    User student = db.users.findByEmailAddress(testData.spongeBob.getEmailAddress());
-    assertThat(student).isNotNull();
-    assertThat(student.getId()).isGreaterThan(0);
+    Optional<User> student = db.users.findByEmailAddress(testData.spongeBob.getEmailAddress());
+    assertThat(student).isPresent();
+    assertThat(student.get().getId()).isGreaterThan(0);
   }
 }
