fc5663e0f0b9b4ed91c9d19f944287cba9151921
JosiahFu <josiahfu@gmail.com>
2023-04-05 21:09:24 -1000

    Remove default exports, temporarily removed `Spinner` page

--- server/src/main/clients/web-browser/src/pages/Nav.tsx
+++ server/src/main/clients/web-browser/src/pages/Nav.tsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom';
 import './Nav.scss';
 import {Outlet} from 'react-router';
 
-export default function Login() {
+export function Nav() {
   return (
     <>
       <header>
--- server/src/main/clients/web-browser/src/pages/Root.tsx
+++ server/src/main/clients/web-browser/src/pages/Root.tsx
@@ -1,6 +1,6 @@
 import './Root.scss';
 
-export default function Root() {
+export function Root() {
   return (
     <>
       <section className="primary-section">
@@ -138,3 +138,78 @@ export default function Root() {
     </>
   );
 }
+
+// import {useEffect, useState} from 'react';
+// import * as protos from '../protos';
+// import {Coordinate, IkigaiReel} from '../IkigaiReel/IkigaiReel';
+
+// export function Spinner() {
+//   const [messageOfTheDay, setMessageOfTheDay] = useState('');
+
+//   const [ikigaiCenter, setIkigaiCenter] = useState<Coordinate | null>(null);
+//   const [ikigaiDistance, setIkigaiDistance] = useState(0);
+//   const [ikigaiSize, setIkigaiSize] = useState(0);
+
+//   function updateIkigaiPosition() {
+//     const main = document.getElementsByTagName('main').item(0) as HTMLElement;
+//     if (main) {
+//       setIkigaiCenter({
+//         x: main.offsetLeft + main.offsetWidth / 2,
+//         y:
+//           main.offsetTop +
+//           main.offsetHeight / 2 +
+//           (Math.min(main.offsetWidth, main.offsetHeight) / 2) * 0.07,
+//       });
+//       setIkigaiSize((Math.min(main.offsetWidth, main.offsetHeight) / 2) * 0.8);
+//       setIkigaiDistance(
+//         (Math.min(main.offsetWidth, main.offsetHeight) / 2) * 0.8 * 0.4
+//       );
+//     }
+//   }
+
+//   // Updates the Ikigai position after layout and on window resize.
+//   useEffect(() => {
+//     updateIkigaiPosition();
+//     window.addEventListener('resize', updateIkigaiPosition);
+//   }, []);
+
+//   // Loads and sets the message of the day.
+//   useEffect(() => {
+//     const motdService = protos.createService(
+//       protos.message_of_the_day.MessageOfTheDayService,
+//       'MessageOfTheDayService'
+//     );
+
+//     motdService
+//       .getMessage(protos.message_of_the_day.MessageRequest.create({}))
+//       .then((response: protos.message_of_the_day.MessageResponse) => {
+//         setMessageOfTheDay(response.message!);
+//       })
+//       .catch((error: Error) => {
+//         setMessageOfTheDay(`${error.name}: ${error.message}`);
+//       });
+//   }, []);
+
+//   return (
+//     <>
+//       <IkigaiReel
+//         id="IkigaiReel"
+//         origin={ikigaiCenter}
+//         size={ikigaiSize}
+//         sizeDelta={ikigaiSize / 6}
+//         distance={ikigaiDistance * 1.1}
+//         distanceDelta={ikigaiDistance / 6}
+//       />
+//       {messageOfTheDay}
+//       <div>
+//         <button>Default</button>
+//         <button className="light">Light</button>
+//         <button className="primary">Primary</button>
+//         <button className="info">Info</button>
+//         <button className="delete">Delete</button>
+//         <button className="prev">Prev</button>
+//         <button className="next">Next</button>
+//       </div>
+//     </>
+//   );
+// }
