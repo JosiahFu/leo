5ee6fb07e2fc172e06a0107e9879616ee949aabc
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-04-18 03:57:36 -0700

    Clean up landing page.

--- server/src/main/clients/web-browser/src/pages/Root.tsx
+++ server/src/main/clients/web-browser/src/pages/Root.tsx
@@ -41,12 +41,8 @@ export function Root() {
   return (
     <>
       <header>
-        <Link
-          to="/"
-          className="header-section header-section-left header-title nav-link"
-        >
+        <Link to="/" className="header-section header-section-left">
           <img src="/logo-white-on-orange.svg" />
-          <div id="site-title">PROJECT LEO</div>
         </Link>
         <div className="header-section header-section-center">
           <Link to="" className="nav-link">
@@ -69,150 +65,176 @@ export function Root() {
         </div>
       </header>
       <main>
-        <section className="primary-section">
-          <h1>Empowering Teachers, Engaging Students</h1>
-          <div className="subtitle">
-            Unlock Your Classroom's Full Potential with Project Leo
+        <section
+          className="primary-section"
+          style={{display: 'flex', alignItems: 'flex-start', gap: '15px'}}
+        >
+          <div>
+            <h1>Empowering&nbsp;Teachers, Engaging&nbsp;Students</h1>
+            <div className="subheading">
+              Unlock Your Classroom's Full Potential with Project Leo
+            </div>
+            <div className="summary">
+              Designed with both teachers and students in mind, Project Leo
+              provides an easy way to incorporate differentiation into your
+              class, while giving students the freedom to pursue projects that
+              truly inspire them. Say goodbye to the monotony of traditional
+              schooling and hello to a purposeful and engaging learning
+              experience.
+            </div>
+            <button className="light" onClick={() => setInterestFormOpen(true)}>
+              I'm interested! Tell me more!
+            </button>
           </div>
-          <p>
-            Designed with both teachers and students in mind, Project Leo
-            provides an easy way to incorporate differentiation into your class,
-            while giving students the freedom to pursue projects that truly
-            inspire them. Say goodbye to the monotony of traditional schooling
-            and hello to a purposeful and engaging learning experience.
-          </p>
-          <button className="light" onClick={() => setInterestFormOpen(true)}>
-            I'm interested! Tell me more!
-          </button>
-          {/* <Carousel />*/}
+          <img src="/main_section_landing_page.png" />
         </section>
-        <section>
-          <h2>What is Project Leo?</h2>
-          <p>
-            Project Leo is an innovative educational tool designed to inspire
-            both teachers and students. With Project Leo, teachers can easily
-            incorporate differentiation into their classes, helping students
-            find their own unique paths to success. By removing the rigidity of
-            traditional classrooms, Project Leo gives students the freedom to
-            pursue their passions and explore their own interests. By engaging
-            students and empowering them to take control of their own learning,
-            Project Leo helps to give school a purpose and prepares them for
-            their future careers.
-          </p>
-          <img />
-          <div className="image-placeholder">[Image here]</div>
+        <img src="/swoop.svg" style={{width: '100%'}} />
+        <section className="details-with-image">
+          <div>
+            <h2>What is Project Leo?</h2>
+            <p>
+              Project Leo is an innovative educational tool designed to inspire
+              both teachers and students. With Project Leo, teachers can easily
+              incorporate differentiation into their classes, helping students
+              find their own unique paths to success. By removing the rigidity
+              of traditional classrooms, Project Leo gives students the freedom
+              to pursue their passions and explore their own interests. By
+              engaging students and empowering them to take control of their own
+              learning, Project Leo helps to give school a purpose and prepares
+              them for their future careers.
+            </p>
+          </div>
+          <img src="/placeholder.jpg" />
         </section>
-        <section>
-          <h2>Why Project Leo?</h2>
-          <p>
-            Project Leo not only provides project ideas that align with school
-            learning objectives but also helps students develop career skills.
-            By incorporating the students' interests, market demands, and
-            skills, Project Leo offers a unique opportunity for students to
-            explore their passions while preparing for their future careers.
-            This approach empowers students to take ownership of their learning
-            and enables them to develop a clear understanding of how their
-            education can contribute to their professional goals.
-          </p>
-          {/* <IkigaiReel /> */}
-          <div className="image-placeholder">[Image here]</div>
-        </section>
-        <section>
-          <h2>How does it work?</h2>
-          <div className="timeline">
-            <div>
-              <h3>Students generate a unique project</h3>
-              <p>
-                Project Leo allows students to take ownership of their learning
-                by generating personalized projects based on their interests.
-                Using AI technology, Project Leo aligns students' interests with
-                class objectives and possible career paths, creating a truly
-                customized and engaging learning experience.
-              </p>
-            </div>
-            {/* <img /> */}
-            <div className="image-placeholder">[Image here]</div>
-            <div>
-              <h3>Students post their progress</h3>
-              <p>
-                Project Leo allows students to take ownership of their learning
-                by generating personalized projects based on their interests.
-                Using AI technology, Project Leo aligns students' interests with
-                class objectives and possible career paths, creating a truly
-                customized and engaging learning experience.
-              </p>
-            </div>
-            {/* <img /> */}
-            <div className="image-placeholder">[Image here]</div>
-            <div>
-              <h3>Teachers and professionals review and provide feedback</h3>
-              <p>
-                Teachers and professionals in related fields can monitor student
-                progress and provide valuable feedback throughout the project
-                process. This collaborative approach promotes a supportive
-                learning environment where students can receive guidance and
-                support from multiple sources, including those with specialized
-                knowledge and expertise. With this approach, students are able
-                to work closely with both their teachers and industry
-                professionals, fostering both academic and personal growth.
-              </p>
-            </div>
-            {/* <img /> */}
-            <div className="image-placeholder">[Image here]</div>
-            <div>
-              <h3>Project Leo outputs final results</h3>
-              <p>
-                Project Leo generates a summary of project results for both
-                students and teachers, providing a clear understanding of
-                project outcomes and serving as a tool for continuous
-                improvement. Students can use the information to reflect on
-                their learning and set future goals, while teachers can easily
-                grade projects and provide feedback to students, promoting a
-                collaborative and effective learning environment.
-              </p>
-            </div>
-            {/* <img src="" alt="" /> */}
-            <div className="image-placeholder">[Image here]</div>
-            <div>
-              <h3>Move on to the next project</h3>
-              <p>
-                Project Leo generates an unlimited amount of engaging project
-                ideas, allowing students to explore new topics and areas of
-                interest, while building upon existing skills. This feature
-                promotes a love of lifelong learning, while keeping students
-                engaged and motivated to continue exploring and learning.
-              </p>
-            </div>
-            {/* <img src="" alt="" /> */}
-            <div className="image-placeholder">[Image here]</div>
+        <section className="details-with-image">
+          <img src="/placeholder.jpg" />
+          <div>
+            <h2>Why Project Leo?</h2>
+            <p>
+              Project Leo not only provides project ideas that align with school
+              learning objectives but also helps students develop career skills.
+              By incorporating the students' interests, market demands, and
+              skills, Project Leo offers a unique opportunity for students to
+              explore their passions while preparing for their future careers.
+              This approach empowers students to take ownership of their
+              learning and enables them to develop a clear understanding of how
+              their education can contribute to their professional goals.
+            </p>
           </div>
         </section>
-        <section>
-          <h2>Our Mission</h2>
-          <p>
-            At Project Leo, our mission is to empower teachers and students by
-            providing a personalized and engaging learning experience. We aim to
-            promote collaboration and accountability by enabling students to
-            generate their own projects, showcase their progress, and receive
-            feedback from teachers. Our goal is to foster a love of lifelong
-            learning by providing a platform for students to explore their
-            passions, build new skills, and prepare for future careers.
-          </p>
-          {/* <img /> */}
-          <div className="image-placeholder">[Image here]</div>
+        <section className="details">
+          <div>
+            <h2>How does it work?</h2>
+            <div className="timeline-with-image">
+              <div>
+                <h3>Students generate a unique project</h3>
+                <p>
+                  Project Leo allows students to take ownership of their
+                  learning by generating personalized projects based on their
+                  interests. Using AI technology, Project Leo aligns students'
+                  interests with class objectives and possible career paths,
+                  creating a truly customized and engaging learning experience.
+                </p>
+              </div>
+              <img src="/placeholder.jpg" width="50%" />
+            </div>
+            <div className="timeline-with-image">
+              <img src="/placeholder.jpg" width="50%" />
+              <div>
+                <h3>Students post their progress</h3>
+                <p>
+                  Project Leo allows students to take ownership of their
+                  learning by generating personalized projects based on their
+                  interests. Using AI technology, Project Leo aligns students'
+                  interests with class objectives and possible career paths,
+                  creating a truly customized and engaging learning experience.
+                </p>
+              </div>
+            </div>
+            <div className="timeline-with-image">
+              <div>
+                <h3>Teachers and professionals review and provide feedback</h3>
+                <p>
+                  Teachers and professionals in related fields can monitor
+                  student progress and provide valuable feedback throughout the
+                  project process. This collaborative approach promotes a
+                  supportive learning environment where students can receive
+                  guidance and support from multiple sources, including those
+                  with specialized knowledge and expertise. With this approach,
+                  students are able to work closely with both their teachers and
+                  industry professionals, fostering both academic and personal
+                  growth.
+                </p>
+              </div>
+              <img src="/placeholder.jpg" width="50%" />
+            </div>
+            <div className="timeline-with-image">
+              <img src="/placeholder.jpg" width="50%" />
+              <div>
+                <h3>Project Leo outputs final results</h3>
+                <p>
+                  Project Leo generates a summary of project results for both
+                  students and teachers, providing a clear understanding of
+                  project outcomes and serving as a tool for continuous
+                  improvement. Students can use the information to reflect on
+                  their learning and set future goals, while teachers can easily
+                  grade projects and provide feedback to students, promoting a
+                  collaborative and effective learning environment.
+                </p>
+              </div>
+            </div>
+            <div className="timeline-with-image">
+              <div>
+                <h3>Move on to the next project</h3>
+                <p>
+                  Project Leo generates an unlimited amount of engaging project
+                  ideas, allowing students to explore new topics and areas of
+                  interest, while building upon existing skills. This feature
+                  promotes a love of lifelong learning, while keeping students
+                  engaged and motivated to continue exploring and learning.
+                </p>
+              </div>
+              <img src="/placeholder.jpg" width="50%" />
+            </div>
+          </div>
         </section>
-        <section>
-          <h2>Contact Us</h2>
-          <p>
-            Interested in learning more about Project Leo? Are you experiencing
-            any site issues? While you are exploring, please send all feedback
-            and issues to Steven Eno -{' '}
-            <a href="mailto:seno@davincischools.org">seno@davincischools.org</a>
-            .
-          </p>
+        <section className="details">
+          <div>
+            <h2>Our Mission</h2>
+            <p>
+              At Project Leo, our mission is to empower teachers and students by
+              providing a personalized and engaging learning experience. We aim
+              to promote collaboration and accountability by enabling students
+              to generate their own projects, showcase their progress, and
+              receive feedback from teachers. Our goal is to foster a love of
+              lifelong learning by providing a platform for students to explore
+              their passions, build new skills, and prepare for future careers.
+            </p>
+          </div>
+          <img src="/placeholder.jpg" />
+        </section>
+        <section className="details">
+          <div>
+            <h2>Contact Us</h2>
+            <p>
+              Interested in learning more about Project Leo? Are you
+              experiencing any site issues? While you are exploring, please send
+              all feedback and issues to Steven Eno -{' '}
+              <a href="mailto:seno@davincischools.org">
+                seno@davincischools.org
+              </a>
+            </p>
+          </div>
         </section>
       </main>
-      <footer>&nbsp;</footer>
+      <footer>
+        <Link
+          to="https://www.freepik.com/free-vector/illustration-gallery-icon_2922280.htm#query=placeholder&position=0&from_view=search&track=sph"
+          className="nav-link"
+        >
+          Placeholder image by rawpixel.com on Freepik
+        </Link>
+      </footer>
       <Modal
         title="I'm interested! Tell me more!"
         width="80%"
