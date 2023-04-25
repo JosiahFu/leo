2a775f89006194822342ba5cb536fdd290db3e36
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-04-25 16:15:03 -0700

    Change layout to latest Figma presentation.

--- server/src/main/clients/web-browser/src/pages/DefaultPageNav.tsx
+++ server/src/main/clients/web-browser/src/pages/DefaultPageNav.tsx
@@ -4,16 +4,20 @@ import {Layout, Menu} from 'antd';
 import {
   AppstoreOutlined,
   DesktopOutlined,
+  HomeOutlined,
   RocketOutlined,
   SettingOutlined,
   UserOutlined,
 } from '@ant-design/icons';
 import {getCurrentUser, logout, Role} from '../utils/authentication';
 import {Link} from 'react-router-dom';
+import {Footer} from 'antd/es/layout/layout';
+import {useState} from 'react';
 
 const {Header, Sider, Content} = Layout;
 
 enum MenuKeys {
+  HOME,
   DASHBOARD,
   PROJECTS,
   MY_PROJECTS,
@@ -28,95 +32,91 @@ enum MenuKeys {
 
 export function DefaultPageNav() {
   const user = getCurrentUser(() => {});
+  const [collapsed, setCollapsed] = useState(false);
 
   return (
     <>
       <Layout>
-        <Header>
-          <div>
-            <Link to="/">
-              <img
-                src="/images/logo-orange-on-white.svg"
-                alt="Project Leo Logo | Go Home"
-              />
-            </Link>
-          </div>
-          <div style={{whiteSpace: 'nowrap'}}>[TODO: Breadcrumbs]</div>
-          <div style={{width: '100%', padding: 0, textAlign: 'right'}}>
-            <span
-              style={{
-                display: user != null ? 'inline' : 'none',
-              }}
-              onClick={() => {
-                logout();
-                window.location.reload();
-              }}
-            >
-              <Link to="/login">Logout</Link>
-            </span>
-            <span
-              style={{
-                display: user == null ? 'inline' : 'none',
-              }}
-            >
-              <Link to="/login">Login</Link>
-            </span>
-          </div>
-        </Header>
-        <Layout>
-          <Sider>
-            <Menu mode="inline">
-              <Menu.Item key={MenuKeys.DASHBOARD} icon={<AppstoreOutlined />}>
-                Dashboard
-              </Menu.Item>
-              <Menu.SubMenu
-                key={MenuKeys.PROJECTS}
-                icon={<RocketOutlined />}
-                title="Projects"
-              >
-                <Menu.Item key={MenuKeys.MY_PROJECTS}>
-                  <Link to="/projects/my-projects">My Projects</Link>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={() => setCollapsed(!collapsed)}
+          className="left-menu"
+        >
+          <Layout>
+            <Header>
+              <Link to="/">
+                <img
+                  src={
+                    collapsed
+                      ? '/images/logo-orange-on-white-circles.svg'
+                      : '/images/logo-orange-on-white.svg'
+                  }
+                  alt="Project Leo Logo | Go Home"
+                />
+              </Link>
+            </Header>
+            <Content>
+              <Menu mode="inline" className="top-menu">
+                <Menu.Item key={MenuKeys.HOME} icon={<HomeOutlined />}>
+                  Home
                 </Menu.Item>
-                <Menu.Item key={MenuKeys.IKIGAI_BUILDER}>
-                  <Link to="/projects/ikigai-builder">Ikigai Builder</Link>
+                <Menu.Item key={MenuKeys.DASHBOARD} icon={<AppstoreOutlined />}>
+                  Dashboard
                 </Menu.Item>
-              </Menu.SubMenu>
-              <Menu.Item key={MenuKeys.INTERNSHIPS} icon={<DesktopOutlined />}>
-                Internships
-              </Menu.Item>
-              <Menu.Item key={MenuKeys.MY_ACCOUNT} icon={<UserOutlined />}>
-                My Account
-              </Menu.Item>
-              <Menu.SubMenu
-                key={MenuKeys.ADMIN}
-                icon={<SettingOutlined />}
-                title="Admin"
-                style={{
-                  display:
-                    user != null && user.roles.has(Role.ADMIN)
-                      ? 'block'
-                      : 'none',
-                }}
-              >
-                <Menu.Item key={MenuKeys.EDIT_DISTRICTS}>
-                  <Link to="/profiles/edit-districts">Edit Districts</Link>
+                <Menu.SubMenu
+                  key={MenuKeys.PROJECTS}
+                  icon={<RocketOutlined />}
+                  title="Projects"
+                >
+                  <Menu.Item key={MenuKeys.MY_PROJECTS}>
+                    <Link to="/projects/my-projects">My Projects</Link>
+                  </Menu.Item>
+                  <Menu.Item key={MenuKeys.IKIGAI_BUILDER}>
+                    <Link to="/projects/ikigai-builder">Ikigai Builder</Link>
+                  </Menu.Item>
+                </Menu.SubMenu>
+                <Menu.Item
+                  key={MenuKeys.INTERNSHIPS}
+                  icon={<DesktopOutlined />}
+                >
+                  Internships
                 </Menu.Item>
-                <Menu.Item key={MenuKeys.EDIT_SCHOOLS}>
-                  <Link to="/profiles/edit-schools">Edit Schools</Link>
+                <Menu.SubMenu
+                  key={MenuKeys.ADMIN}
+                  icon={<SettingOutlined />}
+                  title="Admin"
+                  style={{
+                    display:
+                      user != null && user.roles.has(Role.ADMIN)
+                        ? 'block'
+                        : 'none',
+                  }}
+                >
+                  <Menu.Item key={MenuKeys.EDIT_DISTRICTS}>
+                    <Link to="/profiles/edit-districts">Edit Districts</Link>
+                  </Menu.Item>
+                  <Menu.Item key={MenuKeys.EDIT_SCHOOLS}>
+                    <Link to="/profiles/edit-schools">Edit Schools</Link>
+                  </Menu.Item>
+                  <Menu.Item key={MenuKeys.EDIT_USERS}>
+                    <Link to="/profiles/edit-users">Edit Users</Link>
+                  </Menu.Item>
+                </Menu.SubMenu>
+              </Menu>
+            </Content>
+            <Footer>
+              <Menu mode="inline" className="top-menu">
+                <Menu.Item key={MenuKeys.MY_ACCOUNT} icon={<UserOutlined />}>
+                  My Account
                 </Menu.Item>
-                <Menu.Item key={MenuKeys.EDIT_USERS}>
-                  <Link to="/profiles/edit-users">Edit Users</Link>
-                </Menu.Item>
-              </Menu.SubMenu>
-            </Menu>
-          </Sider>
-          <Content
-            style={{border: '#F0781F solid 1px', padding: 0}}
-            className="content"
-          >
-            <Outlet />
-          </Content>
-        </Layout>
+              </Menu>
+            </Footer>
+          </Layout>
+        </Sider>
+        <Content>
+          <Outlet />
+        </Content>
       </Layout>
     </>
   );
