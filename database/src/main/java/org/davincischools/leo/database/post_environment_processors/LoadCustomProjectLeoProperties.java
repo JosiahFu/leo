cf877d7272d9cb247fc4e97e8c064ae7f6542c1d
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-05-11 10:33:52 -0700

    Allow loading custom Project Leo properties at any time.

--- database/src/main/java/org/davincischools/leo/database/post_environment_processors/LoadCustomProjectLeoProperties.java
+++ database/src/main/java/org/davincischools/leo/database/post_environment_processors/LoadCustomProjectLeoProperties.java
@@ -3,6 +3,8 @@ package org.davincischools.leo.database.post_environment_processors;
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
@@ -11,24 +13,33 @@ import org.springframework.core.io.support.ResourcePropertySource;
 /** Loads additional properties from ${HOME}/project_leo.properties, if present. */
 public class LoadCustomProjectLeoProperties implements EnvironmentPostProcessor {
 
-  @Override
-  public void postProcessEnvironment(
-      ConfigurableEnvironment environment, SpringApplication application) {
+  private static final Logger logger = LogManager.getLogger();
+
+  public static void loadCustomProjectLeoProperties(ConfigurableEnvironment environment) {
+    File projectLeoPropertiesFile =
+        new File(Optional.ofNullable(System.getenv("HOME")).orElse(""), "project_leo.properties");
     try {
-      // Load the additional Project Leo properties file, if it exists.
-      File projectLeoProperties =
-          new File(Optional.ofNullable(System.getenv("HOME")).orElse(""), "project_leo.properties");
-      if (projectLeoProperties.exists()) {
+      // Load the custom Project Leo properties file, if it exists.
+      if (projectLeoPropertiesFile.exists()
+          && !environment.getPropertySources().contains(LoadCustomProjectLeoProperties.class.getName())) {
         environment
             .getPropertySources()
             .addFirst(
                 new ResourcePropertySource(
                     LoadCustomProjectLeoProperties.class.getName(),
-                    projectLeoProperties.toURI().toString()));
+                    projectLeoPropertiesFile.toURI().toString()));
       }
+
+      logger.atInfo().log("Loaded custom Project Leo properties: {}", projectLeoPropertiesFile);
     } catch (IOException e) {
       throw new RuntimeException(
-          "Failed to load additional properties from \"${HOME}/project_leo.properties\".", e);
+          "Failed to load custom Project Leo properties: " + projectLeoPropertiesFile + ".", e);
     }
+  }
+
+  @Override
+  public void postProcessEnvironment(
+      ConfigurableEnvironment environment, SpringApplication application) {
+    loadCustomProjectLeoProperties(environment);
   }
 }
