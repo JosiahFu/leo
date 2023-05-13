c8ef92f1cb998971334a7a25fb79c5929e6b5c94
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-05-12 17:16:37 -0700

    Add user icon drop down menu.

--- server/src/main/clients/web-browser/src/libs/DefaultPage/DefaultPage.tsx
+++ server/src/main/clients/web-browser/src/libs/DefaultPage/DefaultPage.tsx
@@ -7,9 +7,33 @@ import {
   SmileTwoTone,
 } from '@ant-design/icons';
 import {PropsWithChildren} from 'react';
+import {Popover} from 'antd';
+import {Link} from 'react-router-dom';
 
 export function DefaultPage(props: PropsWithChildren<{title: string}>) {
   const user = getCurrentUser(() => {});
+  const avatarPanel = (
+    <>
+      <div className="avatar-panel">
+        <div className="avatar-icon">
+          <SmileTwoTone />
+        </div>
+        <div className="name">
+          <span style={{whiteSpace: 'nowrap'}}>
+            {user?.firstName} {user?.lastName}
+          </span>
+        </div>
+        <div className="email">{user?.emailAddress}</div>
+        <div className="sectionDiv" />
+        <div className="menu">
+          <Link to="/users/my-account">My Account</Link>
+        </div>
+        <div className="menu">
+          <Link to="/users/logout">Logout</Link>
+        </div>
+      </div>
+    </>
+  );
 
   return (
     <>
@@ -19,10 +43,15 @@ export function DefaultPage(props: PropsWithChildren<{title: string}>) {
           <div className="page-account">
             <QuestionCircleOutlined />
             <BellOutlined />
-            <span className="profile-icon">
-              <SmileTwoTone />
-            </span>
-            {user?.firstName} {user?.lastName}
+            <Popover
+              content={avatarPanel}
+              placement="bottomRight"
+              trigger="click"
+            >
+              <span className="avatar-icon">
+                <SmileTwoTone />
+              </span>
+            </Popover>
           </div>
         </div>
         <div className="page-body">{props.children}</div>
