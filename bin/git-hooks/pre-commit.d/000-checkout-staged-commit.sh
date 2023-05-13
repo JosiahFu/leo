074baabb01e62812f7cf5f7f05793eefaab9d2be
Scott A. Hendrickson <sahendrickson@gmail.com>
2023-05-12 23:16:34 -0700

    Fix the issue with pre-commit hooks detaching HEAD.

--- bin/git-hooks/pre-commit.d/000-checkout-staged-commit.sh
+++ bin/git-hooks/pre-commit.d/000-checkout-staged-commit.sh
@@ -8,9 +8,18 @@ commands () {
 
   rm -rf "${STAGED_FILES_TEMP_DIR}"
   mkdir --parents "${STAGED_FILES_TEMP_DIR}"
-  git -C "${STAGED_FILES_TEMP_DIR}" clone --local --no-checkout "${GIT_DIR}" "${STAGED_FILES_TEMP_DIR}"
+
+  # HEAD in the original repository gets updated to point to the staged commit
+  # when there's a checkout in the cloned repository, when it shouldn't. So,
+  # back it up here to restore it after the git operations.
+  OLD_HEAD="$(cat "${GIT_DIR}/HEAD")"
+
+  git clone --no-checkout "${GIT_DIR}" "${STAGED_FILES_TEMP_DIR}"
   git -C "${STAGED_FILES_TEMP_DIR}" checkout "${GIT_HASH}"
   git -C "${STAGED_FILES_TEMP_DIR}" submodule update --init --recursive --depth=1
+
+  # Restore HEAD to its original value.
+  echo -n "${OLD_HEAD}" > "${GIT_DIR}/HEAD"
 }
 OUTPUT="$(commands 2>&1)"
 
