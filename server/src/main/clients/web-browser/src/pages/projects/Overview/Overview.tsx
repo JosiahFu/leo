2031f49c327aaa15a7f3eb867943a828e1708f2d
JosiahFu <josiahfu@gmail.com>
2023-05-19 09:46:52 -0700

    Added project dashboard with guage

--- server/src/main/clients/web-browser/src/pages/projects/Overview/Overview.tsx
+++ server/src/main/clients/web-browser/src/pages/projects/Overview/Overview.tsx
@@ -1,10 +1,21 @@
 import './Overview.scss';
 import {DefaultPage} from '../../../libs/DefaultPage/DefaultPage';
+import {ProjectDashboard} from '../../../libs/projects/ProjectDashboard/ProjectDashboard';
+import {Layout} from 'antd';
+import {Content} from 'antd/es/layout/layout';
 
 export function Overview() {
   return (
     <>
-      <DefaultPage title="Projects" />
+      <DefaultPage title="My Projects">
+        <Layout style={{height: '100%'}}>
+          <Content style={{borderRight: '#F0781F solid 1px', padding: '0.5em'}}>
+            <div style={{width: '100%', height: '100%'}}>
+              <ProjectDashboard />
+            </div>
+          </Content>
+        </Layout>
+      </DefaultPage>
     </>
   );
 }
