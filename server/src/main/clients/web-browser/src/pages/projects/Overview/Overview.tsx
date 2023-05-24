3fedc4cd7586fd7c5b4b91713275b508f3fada71
JosiahFu <josiahfu@gmail.com>
2023-05-24 00:44:42 -0700

    Change title and import

--- server/src/main/clients/web-browser/src/pages/projects/Overview/Overview.tsx
+++ server/src/main/clients/web-browser/src/pages/projects/Overview/Overview.tsx
@@ -2,18 +2,19 @@ import './Overview.scss';
 import {DefaultPage} from '../../../libs/DefaultPage/DefaultPage';
 import {ProjectDashboard} from '../../../libs/projects/ProjectDashboard/ProjectDashboard';
 import {Layout} from 'antd';
-import {Content} from 'antd/es/layout/layout';
 
 export function Overview() {
   return (
     <>
-      <DefaultPage title="My Projects">
+      <DefaultPage title="Projects Overview">
         <Layout style={{height: '100%'}}>
-          <Content style={{borderRight: '#F0781F solid 1px', padding: '0.5em'}}>
+          <Layout.Content
+            style={{borderRight: '#F0781F solid 1px', padding: '0.5em'}}
+          >
             <div style={{width: '100%', height: '100%'}}>
               <ProjectDashboard />
             </div>
-          </Content>
+          </Layout.Content>
         </Layout>
       </DefaultPage>
     </>
